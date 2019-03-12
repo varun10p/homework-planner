fcd5889ae88b1bd58aff83cb7d791d58fe4e396a
varun10p <41707187+varun10p@users.noreply.github.com>
2019-03-11 22:01:59 -0700

    Add files via upload

---
+++ CompSciiA/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+const db = require('./config/keys').MongoURI;
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+app.use(expressLayouts);
+app.set('view engine', 'ejs')
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
---
+++ CompSciiA/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://varun10p:pvcUZDihxtdWQatx@devtable-pc01v.mongodb.net/test?retryWrites=true'
+    
+}
